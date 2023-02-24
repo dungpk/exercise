ac0f18c6afd25e97065752733a2019bf070aaa58
User Name~ <dungphung01041998@gmail.com>
2023-02-24 14:27:51 +0700

    commit convert money

---
+++ Bien-kieudulieu-toantu/convert_money/index.js
@@ -0,0 +1,20 @@
+function convertMoney(){
+    let quantity = document.getElementById("quantity").value;
+    let FromCurrency = document.getElementById("FromCurrency").value;
+    let ToCurrency= document.getElementById("ToCurrency").value;
+    let Result;
+    
+    if (FromCurrency == "USD" && ToCurrency == "VND"){
+        Result = "Result:" + (quantity*23000)+ " Đồng"
+        }
+        else if (FromCurrency == "VND" && ToCurrency == "USD"){
+            Result= "Result:" + (quantity/23000) + " $"
+        }
+        else if (FromCurrency == "VND"){
+            Result= "Result:" + quantity + " Đồng"
+        }
+        else {
+            Result= "Result:" + quantity + " $"
+        }
+            document.getElementById("Result").innerHTML= Result
+    }
